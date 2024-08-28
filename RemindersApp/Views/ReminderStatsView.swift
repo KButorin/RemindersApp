c15c090607075908524af437157a73bd6891efee
BOOTK36 <tanknbk@gmail.com>
2024-08-28 15:32:42 +0300

    Added stats view to home screen

---
+++ RemindersApp/Views/ReminderStatsView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderStatsView.swift
+//  RemindersApp
+//
+//  Created by ESSIP on 28.08.2024.
+//
+
+import SwiftUI
+
+struct ReminderStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderStatsView()
+}
